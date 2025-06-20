5e8172f82fce401422e2fd1f4e42b150f7536223
Blake <810193hongjiayu@gmail.com>
2025-06-20 17:11:31 +0800

    feat: add mui package and set global font

--- frontend/src/pages/_app.tsx
+++ frontend/src/pages/_app.tsx
@@ -1,6 +1,19 @@
 import "@/styles/globals.css";
 import type { AppProps } from "next/app";
+import { createTheme, ThemeProvider } from "@mui/material/styles";
+import CssBaseline from "@mui/material/CssBaseline";
+
+const theme = createTheme({
+  typography: {
+    fontFamily: "monospace", // 設定全局字體為 monospace
+  },
+});
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
+  return (
+    <ThemeProvider theme={theme}>
+      <CssBaseline /> {/* 重置瀏覽器樣式 */}
+      <Component {...pageProps} /> {/* 渲染每個頁面 */}
+    </ThemeProvider>
+  );
 }
